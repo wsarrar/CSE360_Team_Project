a54c836fd01d1e06b033532243deed6165b1de6d
PorterCoder <aportercoder@gmail.com>
2021-04-11 04:01:02 -0700

    Added files and basic layout
    Added files and made basic layout of most of the functionality to be implemented. Specifically VaccineEntry and VaccineRecord.

---
+++ Code/FileController.java
@@ -0,0 +1,5 @@
+//Sole purpose of the class is to ask users for a file to read, load in files, save files, and send back to the main controller
+//will also be the interpreter, converting .csv files from csv format to VaccineEntry format in a VaccineRecord object and vice versa.
+public class FileController {
+
+}
