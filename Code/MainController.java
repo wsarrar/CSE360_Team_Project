aacd43235a7facc59521fd99c88b5305b5132b26
UnfadingRat <79948030+UnfadingRat@users.noreply.github.com>
2021-04-19 13:13:08 -0700

    Update MainController.java

--- Code/MainController.java
+++ Code/MainController.java
@@ -1,5 +1,7 @@
 //Controls the main program, starts up the GUI controller and contains all of the storage for the VaccineRecords and entries
 //that are manipulated in other classes
 public class MainController {
-
+	public static void main(String[] args) {
+		GUIController GUI = new GUIController();
+	}
 }
