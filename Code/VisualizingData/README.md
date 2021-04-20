63657bc05fa22a930631d52336615199fb33114b
Wasfi Sarrar <61529134+wsarrar@users.noreply.github.com>
2021-04-19 22:41:37 -0700

    Delete Code/VisualizingData directory

--- Code/VisualizingData/README.md
+++
@@ -1,1 +0,0 @@
-# Opens the Visualize Data subscreen to create a plot to show the user various views of data.
--- Code/VisualizingData/VisualizeData.java
+++
@@ -1,9 +0,0 @@
-package TP_GUI_APP;
-
-//Creates a plot to show various views of the data
-
-public class VisualizeData 
-{
-	//
-}
-
